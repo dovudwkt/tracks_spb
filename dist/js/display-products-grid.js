2f1daf4304224d0f22d84419256c6af24bb9b8b6
Dovud <dinomov@crucialinteractive.com>
2020-05-20 04:09:02 +0300

    some updates

--- dist/js/display-products-grid.js
+++ dist/js/display-products-grid.js
@@ -2,14 +2,13 @@
 function displayProductGrid(data, appendTo, relPath=''){
     var element;
     for(let i=0; i<data.length;i++){
-        element = ` <div data-id="${i}" data-type="productItem" data-productName="${data[i].model}" class="col-lg-4 col-md-6 col-sm-6">
+        element = ` <div data-id="${i}" data-type="productItem" data-productName="${data[i].model}" class="col-lg-4 col-md-6 col-sm-6 mb-4">
                         <div class="product__item">
                             <div class="product__item__pic set-bg" style='background:url("${relPath}${data[i].imgURL}")'>
-                                <ul class="product__item__pic__hover">
-                                    <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                                </ul>
+                                <div class="product__item__pic__hover">
+                                <span>More</span>
+                                   
+                                </div>
                             </div>
                             <div class="product__item__text">
                                 <h6><a href="./pages/product-details/shop-details.html"  >${data[i].model}</a></h6>
@@ -18,7 +17,10 @@ function displayProductGrid(data, appendTo, relPath=''){
                         </div>
                     </div>` ;
         appendTo.innerHTML += element;
+        // Add shadow
+        document.querySelectorAll('[data-type="productItem"]')[i].style = 'box-shadow: 0px 12px 45px -40px #b98612;';
     }    
+    
 };
 
 // Display all products, all categories
@@ -27,3 +29,19 @@ function displayAllProducts(data, appendTo, relPath=''){
         displayProductGrid(data[i].products, appendTo, relPath);
     }
 }
+
+
+// Display product details in a tabular format || accepts the specific product
+// e.g. const currentProductObj = productsObj[<categoryNo>].products[<productNo>];
+//      const productDetailsObj = currentProductObj.productDetails;
+// displayDetails(productDetailsObj, detailsHolder);
+function displayDetails(product, appendTo){
+    let table = `<table class="table">`;
+    for(property in product){
+        table += `<tr class=""><td><strong> ${property}</strong></td><td>${product[property]}</td> </tr>`
+    }
+    table += `</table>`;
+
+    appendTo.innerHTML = table;
+} 
+
--- dist/js/products-object.js
+++ dist/js/products-object.js
@@ -12,27 +12,45 @@
 
 productsObj = [
 {
+    categoryId: 0,    
     category: 'Экскаваторы',
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     products:[
         {
-            productId: 1,
-            model: 'Экскаватор-погрузчик Komatsu WB93R-52',
+            productId: 0,
+            fullName: 'Экскаватор-погрузчик Komatsu WB93R-52',
+            model: 'Komatsu WB93R-52',
             productQuantity: 12,
             imgURL: 'img/product/1.jpg',
             price: 30,
             currency: '$', 
+            productDetails: {
+                'Общий вес, кг': '7460',
+                'Габаритные размеры, мм':'5895 x 2320 x 3780',
+                'Максимальная высота точки подвеса ковша, мм': '3375', 
+                'Максимальная высота с поднятым ковшом, мм': '4290 / 6405',
+                'Глубина копания, мм': '130 / 6055',
+                'Высота выгрузки, мм': '2720 / 4170',
+            },
         },
         {
-            productId: 2,
+            productId: 1,
             model: 'Hitachi ZX240',
             productQuantity: 10,
             imgURL: 'img/product/2.jpg',
             price: 30,
             currency: '$', 
+            productDetails: {
+                'Общий вес, кг': '456',
+                'Габаритные размеры, мм':'1000 x 2000 x 3780',
+                'Максимальная высота точки подвеса ковша, мм': '5000', 
+                'Максимальная высота с поднятым ковшом, мм': '4290 / 6405',
+                'Глубина копания, мм': '200 / 6058',
+                'Высота выгрузки, мм': '274 / 480',
+            },
         },
-            {
-            productId: 3,
+        {
+            productId: 2,
             model: 'Volvo ЕС-210',
             productQuantity: 6,
             imgURL: 'img/product/3.jpg',
@@ -40,7 +58,7 @@ productsObj = [
             currency: '$', 
         },
         {
-            productId: 4,
+            productId: 3,
             model: 'Volvo ЕС-180',
             productQuantity: 4,
             imgURL: 'img/product/1.jpg',
@@ -51,19 +69,28 @@ productsObj = [
 },
 
 {
+    categoryId: 1,   
     category: 'Экскаваторы погрузчики',
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     products:[
         {
-            productId: 3,
+            productId: 0,
             model: 'Volvo BL 71',
             productQuantity: 6,
             imgURL: 'img/product/3.jpg',
             price: 30,
             currency: '$', 
+            productDetails: {
+                'Общий вес, кг': '456',
+                'Габаритные размеры, мм':'1000 x 2000 x 3780',
+                'Максимальная высота точки подвеса ковша, мм': '5000', 
+                'Максимальная высота с поднятым ковшом, мм': '4290 / 6405',
+                'Глубина копания, мм': '200 / 6058',
+                'Высота выгрузки, мм': '274 / 480',
+            },
         },
         {
-            productId: 4,
+            productId: 1,
             model: 'Volvo ЕС-180',
             productQuantity: 4,
             imgURL: 'img/product/2.jpg',
@@ -73,12 +100,13 @@ productsObj = [
     ]
 },
 
-    {
+{    
+    categoryId: 2,
     category: 'Ивеки',
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     products: [
         {
-            productId: 3,
+            productId: 0,
             model: '3-х и 4-х ясные',
             productQuantity: 10,
             imgURL: 'img/product/1.jpg',
@@ -87,6 +115,7 @@ productsObj = [
         },
     ]
 },
+
 ]
 
 // getNumProducts(productsObj[2].products)
@@ -102,6 +131,6 @@ getTotalNumProducts = data => {
     }
     return num;
 }
-console.log(getNumProducts(productsObj[1].products))
-console.log(getTotalNumProducts(productsObj))
+// console.log(getNumProducts(productsObj[1].products))
+// console.log(getTotalNumProducts(productsObj))
 
