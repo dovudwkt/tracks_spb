5762cc5729f11b9e9443ff4eca2326a2cc5cecf0
Dovud <dinomov@crucialinteractive.com>
2020-05-20 04:54:44 +0300

    added category and product IDs with page names

--- dist/js/products-object.js
+++ dist/js/products-object.js
@@ -134,3 +134,93 @@ getTotalNumProducts = data => {
 // console.log(getNumProducts(productsObj[1].products))
 // console.log(getTotalNumProducts(productsObj))
 
+
+
+
+/*CategoryName----categoryId--------PageName-----------
+Автогрейдеры----------0           category-0.html
+Автовышки-------------1           category-1.html
+Автокраны-------------2           category-2.html
+Асфальто-уклaдчики----3           category-3.html
+Бульдозеры------------4           category-4.html
+Дорожная фреза--------5           category-5.html
+Катки-----------------6           category-6.html   
+Манипуляторы----------7           category-7.html
+Погрузчики------------8           category-8.html
+Самосвалы-------------9           category-9.html
+Тралы----------------10          category-10.html
+Шаланды--------------11          category-11.html
+Экскаваторы----------12          category-12.html
+
+
+example:
+
+<li><a href="pages/category-0.html">Автогрейдеры</a></li>
+<li><a href="pages/category-1.html">Автовышки</a></li>
+<li><a href="pages/category-2.html">Автокраны</a></li>
+<li><a href="pages/category-3.html">Асфальто-уклaдчики</a></li>
+<li><a href="pages/category-4.html">Бульдозеры</a></li>
+<li><a href="pages/category-5.html">Дорожная фреза</a></li>
+<li><a href="pages/category-6.html">Катки</a></li>
+<li><a href="pages/category-7.html">Манипуляторы</a></li>
+<li><a href="pages/category-8.html">Погрузчики</a></li>
+<li><a href="pages/category-9.html">Самосвалы</a></li>
+<li><a href="pages/category-10.html">Тралы</a></li>
+<li><a href="pages/category-11.html">Шаланды</a></li>
+<li><a href="pages/category-12.html">Экскаваторы</a></li>
+
+
+
+---ProductName--------------------productId----categoryId-----siteName
+
+Асфальто-укладчик Volvo P 7820 C ABG----0          3         product-3-0.html
+Асфальто-укладчик Volvo ABG 6870--------1          3         product-3-1.html
+
+Бульдозер CATERPILLAR D5R---------------0          4         product-4-1.html
+Бульдозер John Deere 750J---------------1          4         product-4-1.html
+
+Дорожная фреза WIRTGEN W 1000-----------0          5         product-5-0.html
+
+Асфальтовый каток BW 202 AD-4-----------0          6         product-6-0.html
+Асфальтовый каток Shantui-sd 16---------1          6         product-6-1.html
+Грунтовый каток CAT CS56B---------------2          6         product-6-2.html
+Грунтовый каток Volvo SD 160 DX---------3          6         product-6-3.html
+Грунтовый каток CATERPILLAR CS56--------4          6         product-6-4.html
+
+погрузчик JCB 4CX-----------------------0          8         product-8-0.html
+Телескопический погрузчик JCB 540-------1          8         product-8-1.html
+Телескопический погрузчик Manitou 2150--2          8         product-8-2.html
+Экскаватор-погрузчик Volvo BL 71--------3          8         product-8-3.html
+Фронтальный погрузчик Volvo L 60F-------4          8         product-8-4.html
+Фронтальный погрузчик JCB 436ZX---------5          8         product-8-5.html
+Мини- погрузчик Bobcat S 220------------6          8         product-8-6.html
+
+Трал для перевозки негабаритных грузов--2          10        product-10-0.html
+
+Экскаватор Hitachi ZX200----------------0          12        product-12-0.html
+Экскаватор Hitachi ZX240----------------1          12        product-12-1.html
+Экскаватор Volvo ЕС-210-----------------2          12        product-12-2.html
+Экскаватор Volvo ЕС-180-----------------3          12        product-12-3.html
+Колесные экскаватор Volvo ЕС-210--------4          12        product-12-4.html
+Колесные экскаватор Volvo ЕС-180--------5          12        product-12-5.html
+
+
+
+
+
+
+
+
+
+
+
+Самосвал Ивеки 3-х и 4-х ясные----------0          9         product-9-1.html
+
+
+
+
+
+
+
+
+*/
