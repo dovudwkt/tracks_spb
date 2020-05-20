2a3c6dcb3b7947037f1b536379fcff42c19c5955
Dovud <dinomov@crucialinteractive.com>
2020-05-21 02:10:10 +0300

    added and indexed all products data

--- dist/js/display-products-grid.js
+++ dist/js/display-products-grid.js
@@ -1,32 +1,46 @@
 // Display products of a particular category
 function displayProductGrid(data, appendTo, relPath=''){
     var element;
-    for(let i=0; i<data.length;i++){
-        element = ` <div data-id="${i}" data-type="productItem" data-productName="${data[i].model}" class="col-lg-4 col-md-6 col-sm-6 mb-4">
-                        <div class="product__item">
-                            <div class="product__item__pic set-bg" style='background:url("${relPath}${data[i].imgURL}")'>
-                                <div class="product__item__pic__hover">
-                                <span>More</span>
-                                   
+    if(data.length !== 0){
+        for(let i=0; i<data.length;i++){
+            element = ` <div data-id="${i}" data-type="productItem" data-productName="${data[i].model}" class="col-lg-4 col-md-6 col-sm-6 mb-4">
+                            <div class="product__item">
+                                <div class="product__item__pic set-bg" style='background:url("${relPath}${data[i].imgURL}")'>
+                                    <div class="product__item__pic__hover">
+                                    <span>More</span>
+                                    
+                                    </div>
+                                </div>
+                                <div class="product__item__text">
+                                    <h6><a href="./pages/product-details/shop-details.html"  >${data[i].fullName}</a></h6>
+                                    <h5><span style="float:left;">${data[i].currency}${data[i].price}</span><span style="float:right;">смена 8(м/ч)</span></h5>
                                 </div>
                             </div>
-                            <div class="product__item__text">
-                                <h6><a href="./pages/product-details/shop-details.html"  >${data[i].model}</a></h6>
-                                <h5><span style="float:left;">${data[i].currency}${data[i].price}</span><span style="float:right;">смена 8(м/ч)</span></h5>
-                            </div>
-                        </div>
-                    </div>` ;
-        appendTo.innerHTML += element;
-        // Add shadow
-        document.querySelectorAll('[data-type="productItem"]')[i].style = 'box-shadow: 0px 12px 45px -40px #b98612;';
-    }    
+                        </div>` ;
+            appendTo.innerHTML += element;
+            // Add shadow
+            document.querySelectorAll('[data-type="productItem"]')[i].style = 'box-shadow: 0px 12px 45px -40px #b98612;';
+        }   
+        // Add pagination buttons
+        // appendTo.innerHTML += 
+        //                 `<div class="product__pagination">
+        //                     <a href="#">1</a>
+        //                     <a href="#">2</a>
+        //                     <a href="#">3</a>
+        //                     <a href="#"><i class="fa fa-long-arrow-right"></i></a>
+        //                 </div>`;
+    }else{
+        appendTo.innerHTML = "<h3 style='color:#b2b2b2'>No products found</h3>";
+    }
+     
     
 };
 
 // Display all products, all categories
 function displayAllProducts(data, appendTo, relPath=''){
     for(let i=0; i<data.length; i++){
-        displayProductGrid(data[i].products, appendTo, relPath);
+        if(data[i].products.length > 0)
+            displayProductGrid(data[i].products, appendTo, relPath);
     }
 }
 
--- dist/js/products-object.js
+++ dist/js/products-object.js
@@ -13,108 +13,368 @@
 productsObj = [
 {
     categoryId: 0,    
-    category: 'Экскаваторы',
-    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-    products:[
-        {
-            productId: 0,
-            fullName: 'Экскаватор-погрузчик Komatsu WB93R-52',
-            model: 'Komatsu WB93R-52',
-            productQuantity: 12,
-            imgURL: 'img/product/1.jpg',
-            price: 30,
-            currency: '$', 
-            productDetails: {
-                'Общий вес, кг': '7460',
-                'Габаритные размеры, мм':'5895 x 2320 x 3780',
-                'Максимальная высота точки подвеса ковша, мм': '3375', 
-                'Максимальная высота с поднятым ковшом, мм': '4290 / 6405',
-                'Глубина копания, мм': '130 / 6055',
-                'Высота выгрузки, мм': '2720 / 4170',
-            },
-        },
-        {
-            productId: 1,
-            model: 'Hitachi ZX240',
-            productQuantity: 10,
-            imgURL: 'img/product/2.jpg',
-            price: 30,
-            currency: '$', 
-            productDetails: {
-                'Общий вес, кг': '456',
-                'Габаритные размеры, мм':'1000 x 2000 x 3780',
-                'Максимальная высота точки подвеса ковша, мм': '5000', 
-                'Максимальная высота с поднятым ковшом, мм': '4290 / 6405',
-                'Глубина копания, мм': '200 / 6058',
-                'Высота выгрузки, мм': '274 / 480',
-            },
-        },
-        {
-            productId: 2,
-            model: 'Volvo ЕС-210',
-            productQuantity: 6,
-            imgURL: 'img/product/3.jpg',
-            price: 30,
-            currency: '$', 
-        },
-        {
-            productId: 3,
-            model: 'Volvo ЕС-180',
-            productQuantity: 4,
-            imgURL: 'img/product/1.jpg',
-            price: 30,
-            currency: '$', 
-        }
-    ]
+    category: 'Автогрейдеры',
+    products:[]
 },
 
 {
     categoryId: 1,   
-    category: 'Экскаваторы погрузчики',
-    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-    products:[
-        {
-            productId: 0,
-            model: 'Volvo BL 71',
-            productQuantity: 6,
-            imgURL: 'img/product/3.jpg',
-            price: 30,
-            currency: '$', 
-            productDetails: {
-                'Общий вес, кг': '456',
-                'Габаритные размеры, мм':'1000 x 2000 x 3780',
-                'Максимальная высота точки подвеса ковша, мм': '5000', 
-                'Максимальная высота с поднятым ковшом, мм': '4290 / 6405',
-                'Глубина копания, мм': '200 / 6058',
-                'Высота выгрузки, мм': '274 / 480',
-            },
-        },
-        {
-            productId: 1,
-            model: 'Volvo ЕС-180',
-            productQuantity: 4,
-            imgURL: 'img/product/2.jpg',
-            price: 50,
-            currency: '$', 
-        }
-    ]
+    category: 'Автовышки',
+    details: ['', '', ''],
+    products:[]
 },
 
 {    
     categoryId: 2,
-    category: 'Ивеки',
-    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    category: 'Автокраны',
+    details: ['', '', ''],
+    products: []
+    
+},
+{    
+    categoryId: 3,
+    category: 'Асфальто-уклaдчики',
+    details: ['', '', ''],
     products: [
         {
             productId: 0,
-            model: '3-х и 4-х ясные',
+            model: 'Volvo P 7820 C ABG',
+            fullName: 'Асфальто-укладчик Volvo P 7820 C ABG',
+            productURL: 'product-0-3.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 1,
+            model: 'Volvo ABG 6870',
+            fullName: 'Асфальто-укладчик Volvo ABG 6870',
+            productURL: 'product-1-3.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+    ]  
+},
+{    
+    categoryId: 4,
+    category: 'Бульдозеры',
+    details: ['', '', ''],
+    products: [
+        {
+            productId: 0,
+            model: 'CATERPILLAR D5R',
+            fullName: 'Бульдозер CATERPILLAR D5R',
+            productURL: 'product-0-4.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 1,
+            model: 'John Deere 7',
+            fullName: 'Бульдозер John Deere 7',
+            productURL: 'product-1-4.html',
             productQuantity: 10,
             imgURL: 'img/product/1.jpg',
             price: 10,
             currency: '$',
         },
     ]
+    
 },
+{    
+    categoryId: 5,
+    category: 'Дорожная фреза',
+    details: ['', '', ''],
+    products: [
+        {
+            productId: 0,
+            model: 'WIRTGEN W 1000',
+            fullName: 'Дорожная фреза WIRTGEN W 1000',
+            productURL: 'product-0-5.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+
+    ]
+},
+{
+    categoryId: 6,
+    category: 'Катки',
+    details: ['', '', ''],
+    products: [
+        {
+            productId: 0,
+            model: 'BW 202 AD-4',
+            fullName: 'Асфальтовый каток BW 202 AD-4',
+            productURL: 'product-0-6.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 1,
+            model: 'Shantui-sd 16',
+            fullName: 'Асфальтовый каток Shantui-sd 16',
+            productURL: 'product-1-6.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 2,
+            model: 'CAT CS56B',
+            fullName: 'Асфальтовый каток CAT CS56B',
+            productURL: 'product-2-6.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 3,
+            model: 'Volvo SD 160 DX',
+            fullName: 'Асфальтовый каток Volvo SD 160 DX',
+            productURL: 'product-3-6.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 4,
+            model: 'CATERPILLAR CS56',
+            fullName: 'Асфальтовый каток CATERPILLAR CS56',
+            productURL: 'product-4-6.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+       
+    ]
+    
+},
+{    
+    categoryId: 7,
+    category: 'Манипуляторы',
+    details: ['', '', ''],
+    products: []
+    
+},
+{    
+    categoryId: 8,
+    category: 'Погрузчики',
+    details: ['', '', ''],
+    products: [
+        {
+            productId: 0,
+            model: 'JCB 4CX',
+            fullName: 'Погрузчик JCB 4CX',
+            productURL: 'product-0-8.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 1,
+            model: 'JCB 540',
+            fullName: 'Телескопический погрузчик JCB 540',
+            productURL: 'product-1-8.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 2,
+            model: 'Manitou 2150',
+            fullName: 'Телескопический погрузчик JManitou 2150',
+            productURL: 'product-2-8.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 3,
+            model: 'Volvo BL 71',
+            fullName: 'Экскаватор-погрузчик Volvo BL 71',
+            productURL: 'product-3-8.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 4,
+            model: 'Volvo L 60F',
+            fullName: 'Фронтальный погрузчик Volvo L 60F',
+            productURL: 'product-4-8.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 5,
+            model: 'JCB 436ZX',
+            fullName: 'Фронтальный погрузчик JCB 436ZX',
+            productURL: 'product-5-8.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 6,
+            model: 'Bobcat S 220',
+            fullName: 'Мини-погрузчик Bobcat S 220',
+            productURL: 'product-6-8.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+       
+    ]
+    
+},
+{    
+    categoryId: 9,
+    category: 'Самосвалы',
+    details: ['', '', ''],
+    products: [
+        {
+            productId: 0,
+            model: 'Самосвал Ивеки 3-х и 4-х ясные',
+            fullName: 'Самосвал Ивеки 3-х и 4-х ясные',
+            productURL: 'product-0-9.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+       
+    ]
+    
+},
+{    
+    categoryId: 10,
+    category: 'Тралы',
+    details: ['', '', ''],
+    products: [
+        {
+            productId: 0,
+            model: 'Трал для перевозки негабаритных грузов',
+            fullName: 'Трал для перевозки негабаритных грузов',
+            productURL: 'product-0-10.html',
+            productQuantity: 1,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+       
+    ]
+    
+},
+{    
+    categoryId: 11,
+    category: 'Шаланды',
+    details: ['', '', ''],
+    products: [
+        // {
+        //     productId: 0,
+        //     model: '',
+        //     fullName: '',
+        //     productURL: 'product-0-11.html',
+        //     productQuantity: 0,
+        //     imgURL: 'img/product/1.jpg',
+        //     price: 10,
+        //     currency: '$',
+        // },
+       
+    ] 
+},
+{    
+    categoryId: 12,
+    category: 'Экскаваторы',
+    details: ['', '', ''],
+    products: [
+        {
+            productId: 0,
+            model: 'Hitachi ZX200',
+            fullName: 'Экскаватор Hitachi ZX200',
+            productURL: 'product-0-12.html',
+            productQuantity: 0,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 1,
+            model: 'Hitachi ZX240',
+            fullName: 'Экскаватор Hitachi ZX240',
+            productURL: 'product-1-12.html',
+            productQuantity: 2,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 2,
+            model: 'Volvo ЕС-210',
+            fullName: 'Экскаватор Volvo ЕС-210',
+            productURL: 'product-2-12.html',
+            productQuantity: 1,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 3,
+            model: 'Volvo ЕС-180',
+            fullName: 'Экскаватор Volvo ЕС-180',
+            productURL: 'product-3-12.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 4,
+            model: 'Volvo ЕС-210',
+            fullName: 'Колесный экскаватор Volvo ЕС-210',
+            productURL: 'product-4-12.html',
+            productQuantity: 0,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+        {
+            productId: 5,
+            model: 'Volvo ЕС-180',
+            fullName: 'Колесный экскаватор Volvo ЕС-180',
+            productURL: 'product-5-12.html',
+            productQuantity: 10,
+            imgURL: 'img/product/1.jpg',
+            price: 10,
+            currency: '$',
+        },
+       
+    ] 
+},
+
+
 
 ]
 
@@ -137,24 +397,8 @@ getTotalNumProducts = data => {
 
 
 
-/*CategoryName----categoryId--------PageName-----------
-Автогрейдеры----------0           category-0.html
-Автовышки-------------1           category-1.html
-Автокраны-------------2           category-2.html
-Асфальто-уклaдчики----3           category-3.html
-Бульдозеры------------4           category-4.html
-Дорожная фреза--------5           category-5.html
-Катки-----------------6           category-6.html   
-Манипуляторы----------7           category-7.html
-Погрузчики------------8           category-8.html
-Самосвалы-------------9           category-9.html
-Тралы----------------10          category-10.html
-Шаланды--------------11          category-11.html
-Экскаваторы----------12          category-12.html
 
-
-example:
-
+/*
 <li><a href="pages/category-0.html">Автогрейдеры</a></li>
 <li><a href="pages/category-1.html">Автовышки</a></li>
 <li><a href="pages/category-2.html">Автокраны</a></li>
@@ -170,57 +414,57 @@ example:
 <li><a href="pages/category-12.html">Экскаваторы</a></li>
 
 
+--CategoryName----categoryId--------PageName-----------
+
+Автогрейдеры----------0           category-0.html
+Автовышки-------------1           category-1.html
+Автокраны-------------2           category-2.html
+Асфальто-уклaдчики----3           category-3.html
+Бульдозеры------------4           category-4.html
+Дорожная фреза--------5           category-5.html
+Катки-----------------6           category-6.html   
+Манипуляторы----------7           category-7.html
+Погрузчики------------8           category-8.html
+Самосвалы-------------9           category-9.html
+Тралы----------------10          category-10.html
+Шаланды--------------11          category-11.html
+Экскаваторы----------12          category-12.html
+
 
 ---ProductName--------------------productId----categoryId-----siteName
 
-Асфальто-укладчик Volvo P 7820 C ABG----0          3         product-3-0.html
-Асфальто-укладчик Volvo ABG 6870--------1          3         product-3-1.html
+Асфальто-укладчик Volvo P 7820 C ABG----0          3         product-0-3.html
+Асфальто-укладчик Volvo ABG 6870--------1          3         product-1-3.html
 
-Бульдозер CATERPILLAR D5R---------------0          4         product-4-1.html
-Бульдозер John Deere 750J---------------1          4         product-4-1.html
+Бульдозер CATERPILLAR D5R---------------0          4         product-0-4.html
+Бульдозер John Deere 750J---------------1          4         product-1-4.html
 
-Дорожная фреза WIRTGEN W 1000-----------0          5         product-5-0.html
+Дорожная фреза WIRTGEN W 1000-----------0          5         product-0-5.html
 
-Асфальтовый каток BW 202 AD-4-----------0          6         product-6-0.html
-Асфальтовый каток Shantui-sd 16---------1          6         product-6-1.html
-Грунтовый каток CAT CS56B---------------2          6         product-6-2.html
-Грунтовый каток Volvo SD 160 DX---------3          6         product-6-3.html
-Грунтовый каток CATERPILLAR CS56--------4          6         product-6-4.html
+Асфальтовый каток BW 202 AD-4-----------0          6         product-0-6.html
+Асфальтовый каток Shantui-sd 16---------1          6         product-1-6.html
+Грунтовый каток CAT CS56B---------------2          6         product-2-6.html
+Грунтовый каток Volvo SD 160 DX---------3          6         product-3-6.html
+Грунтовый каток CATERPILLAR CS56--------4          6         product-4-6.html
 
-погрузчик JCB 4CX-----------------------0          8         product-8-0.html
-Телескопический погрузчик JCB 540-------1          8         product-8-1.html
-Телескопический погрузчик Manitou 2150--2          8         product-8-2.html
-Экскаватор-погрузчик Volvo BL 71--------3          8         product-8-3.html
-Фронтальный погрузчик Volvo L 60F-------4          8         product-8-4.html
-Фронтальный погрузчик JCB 436ZX---------5          8         product-8-5.html
-Мини- погрузчик Bobcat S 220------------6          8         product-8-6.html
+погрузчик JCB 4CX-----------------------0          8         product-0-8.html
+Телескопический погрузчик JCB 540-------1          8         product-1-8.html
+Телескопический погрузчик Manitou 2150--2          8         product-2-8.html
+Экскаватор-погрузчик Volvo BL 71--------3          8         product-3-8.html
+Фронтальный погрузчик Volvo L 60F-------4          8         product-4-8.html
+Фронтальный погрузчик JCB 436ZX---------5          8         product-5-8.html
+Мини- погрузчик Bobcat S 220------------6          8         product-6-8.html
 
-Трал для перевозки негабаритных грузов--2          10        product-10-0.html
+Самосвал Ивеки 3-х и 4-х ясные----------0          9         product-0-9.html
 
-Экскаватор Hitachi ZX200----------------0          12        product-12-0.html
-Экскаватор Hitachi ZX240----------------1          12        product-12-1.html
-Экскаватор Volvo ЕС-210-----------------2          12        product-12-2.html
-Экскаватор Volvo ЕС-180-----------------3          12        product-12-3.html
-Колесные экскаватор Volvo ЕС-210--------4          12        product-12-4.html
-Колесные экскаватор Volvo ЕС-180--------5          12        product-12-5.html
+Трал для перевозки негабаритных грузов--2          10        product-0-10.html
 
-
-
-
-
-
-
-
-
-
-
-Самосвал Ивеки 3-х и 4-х ясные----------0          9         product-9-1.html
-
-
-
-
-
-
+Экскаватор Hitachi ZX200----------------0          12        product-0-12.html
+Экскаватор Hitachi ZX240----------------1          12        product-1-12.html
+Экскаватор Volvo ЕС-210-----------------2          12        product-2-12.html
+Экскаватор Volvo ЕС-180-----------------3          12        product-3-12.html
+Колесные экскаватор Volvo ЕС-210--------4          12        product-4-12.html
+Колесные экскаватор Volvo ЕС-180--------5          12        product-5-12.html
 
 
 */
