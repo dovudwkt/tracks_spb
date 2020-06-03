ab4d23777706cf141d92d719a9799915774e835c
Dovud <dinomov@crucialinteractive.com>
2020-06-03 17:12:15 +0300

    working on facilities page

--- dist/js/facilities-obj.js
+++ dist/js/facilities-obj.js
@@ -1,25 +1,29 @@
 
 facilitiesObj = [
     {
-        name: "Администрация МО Сосновоборский городской округ Ленинградской области ",    
-        description: 'Строительство кладбища «Воронка 2» Площадь застройки 10300 м2 Адрес объекта: Ленинградская область, г.Сосновый Бор.',
+        customer: "Администрация МО Сосновоборский городской округ Ленинградской области ",    
+        description: 'Строительство кладбища «Воронка 2» Площадь застройки 10300 м2.',
+        address: 'Ленинградская область, г.Сосновый Бор',
         dateFrom: '22 апреля 2015 г.',
         dateTo: '05 октября 2015 г.', 
     },
     {
-        name: "Санкт-Петербургское ГКУ «Организатор перевозок»",    
-        description: 'Выполнение работ по подключению к наружным сетям канализации и системе коммунального водоснабжения (реконструкция) автобусной станции. Адрес объекта: .г.Ломоносов, Привокзальная улица дом 1',
+        customer: "Санкт-Петербургское ГКУ «Организатор перевозок»",    
+        description: 'Выполнение работ по подключению к наружным сетям канализации и системе коммунального водоснабжения (реконструкция) автобусной станции.',
+        address: 'г.Ломоносов, Привокзальная улица дом 1',
         dateFrom: '1 сентября 2015 г. ',
         dateTo: '18 декабря 2015 г.', 
     },
+    {
+        customer: '',    
+        description: '',
+        address: '',
+        dateFrom: '',
+        dateTo: '', 
+    },
+
 
 ]// end of object
-
-
-
-
-
-
 
 
 
@@ -32,15 +36,16 @@ function displayFacilities(appendTo){
         for(let i=0; i<facilitiesObj.length;i++){
             element = `<div class="col-lg-6 col-md-6 col-sm-6">
                             <div class="blog__item">
-                                <div class="blog__item__pic">
-                                    <img src="" alt="">
-                                </div>
                                 <div class="blog__item__text">
                                     <ul>
                                         <li><i class="fa fa-calendar-o"></i>  ${facilitiesObj[i].dateFrom} - ${facilitiesObj[i].dateTo} </li>
                                     </ul>
-                                    <h5><a href="#">${facilitiesObj[i].name} </a></h5>
+                                    <h5><a >${facilitiesObj[i].customer} </a></h5>
                                     <p>${facilitiesObj[i].description}</p>
+                                    <ul>
+                                        <li><i class="fa fa-map-marker"></i> ${facilitiesObj[i].address}</li>
+                                    </ul>
+                                    
                                 </div>
                             </div>
                         </div>`;
